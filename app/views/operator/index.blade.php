a13d06d718c759d3d7594410161ef780bbb67065
Adrian <adrianabellanosa1998@gmail.com>
2020-07-21 09:29:42 +0800

    remove inbound entries

--- app/views/operator/index.blade.php
+++ app/views/operator/index.blade.php
@@ -32,18 +32,7 @@
 						</span>
 					</a>	
 				</div>
-				<div class="column small-11 small-centered large-4 large-uncentered home-box">
-					<a href="{{ URL::to('fs/inbound') }}" class="home-btn">
-						<div class="home-circle inb-circle">
-							<i class="fa fa-cubes fa-2x"></i>
-						</div>
-
-						<span class="home-btn-text">
-							<h4>{{ $inbound_count }}</h4>
-							INBOUND ENTRIES
-						</span>	
-					</a>	
-				</div>
+				
 			</div>
 			<div class="row">
 				<div class="column small-12 light-box margin-top-20">
